2b6fbe0c25fc73d248560820c36312248542b6f8
hao-np <2795388181@qq.com>
2024-12-12 13:13:20 +0800

    Site updated: YYYY-12-Dec 12, 2024 13:13:19

--- js/random.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var posts = [
-        'https://haonp.top/2024/12/01/Hexo-Butterfly主题美化记录/',
-        'https://haonp.top/2024/11/30/安卓设备Cloudflare内网穿透/',
-        // ...其他文章路径
-    ];
-    var randomPost = posts[Math.floor(Math.random() * posts.length)];
-    window.location.href = randomPost;
-});
