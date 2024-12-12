5f2b278bcdaad7856f782cb22e16d01b7c82e323
hao-np <2795388181@qq.com>
2024-12-12 12:00:57 +0800

    Site updated: YYYY-12-Dec 12, 2024 12:00:57

--- js/random.js
+++ js/random.js
@@ -1,14 +1,9 @@
-hexo.extend.generator.register('random', function (locals) {
-    const config = hexo.config.random || {};
-    const posts = [];
-    for (const post of locals.posts.data) {
-        if (post.random !== false) posts.push(post.path);
-    }
-    return {
-        path: config.path || 'js/random.js',
-        data: `var posts=${JSON.stringify(posts)};
-               function toRandomPost(){
-                   window.open('/' + posts[Math.floor(Math.random() * posts.length)], "_self");
-               };`
-    };
+document.addEventListener('DOMContentLoaded', function() {
+    var posts = [
+        'https://haonp.top/2024/12/01/Hexo-Butterfly主题美化记录/',
+        'https://haonp.top/2024/11/30/安卓设备Cloudflare内网穿透/',
+        // ...其他文章路径
+    ];
+    var randomPost = posts[Math.floor(Math.random() * posts.length)];
+    window.location.href = randomPost;
 });
