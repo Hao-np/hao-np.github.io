ddcfa25010202549720157be903c924bb67e76d0
Hao <2795388181@qq.com>
2024-12-15 18:04:46 +0800

    更新 title.js

--- js/title.js
+++ js/title.js
@@ -8,7 +8,7 @@ document.addEventListener('visibilitychange', function () {
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖抓到你啦～';
+    document.title = '😋抓到你啦～';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
