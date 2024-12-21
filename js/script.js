703755e07bf724aa26a891ea4afb869144debd14
hao-np <2795388181@qq.com>
2024-12-21 14:27:36 +0800

    Site updated: YYYY-12-Dec 21, 2024 14:27:35

--- js/script.js
+++ js/script.js
@@ -1,37 +1,18 @@
-/*var JsHDued=function(v){try {new Function(v)();} catch(e) {setTimeout(function (){JsHDued(v);},100);};};
-JsHDued(`document.getElementById("footer-wrap").style.background="#00FFFFFF";`);
-JsHDued(`document.getElementById("footer-wrap").style.color="#424242";`);
-JsHDued(`document.getElementsByTagName("a")[130].style.color="#424242";`);
-JsHDued(`document.getElementsByTagName("a")[129].style.color="#424242";`);
-JsHDued(`document.getElementsByTagName("a")[131].style.color="#424242";`);
-JsHDued(`document.getElementsByTagName("a")[132].style.color="#424242";`);
-*/
-
-/*建站时间js*/
-var blog_running_years = document.getElementById("blog_running_years");
-var blog_running_months = document.getElementById("blog_running_months");
 var blog_running_days = document.getElementById("blog_running_days");
 var blog_running_hours = document.getElementById("blog_running_hours");
 var blog_running_mins = document.getElementById("blog_running_mins");
 var blog_running_secs = document.getElementById("blog_running_secs");
 
 function refresh_blog_running_time() {
-    var start = new Date(2022, 9, 01, 0, 0, 0); // 建站日期
-    var now = new Date();
-    var time = now - start;
-    var years = parseInt(time / (365 * 24 * 60 * 60 * 1000));
-    var months = parseInt((time % (365 * 24 * 60 * 60 * 1000)) / (30 * 24 * 60 * 60 * 1000));
-    var days = parseInt((time % (30 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000));
-    var hours = parseInt((time % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
-    var minutes = parseInt((time % (60 * 60 * 1000)) / (60 * 1000));
-    var seconds = parseInt((time % (60 * 1000)) / 1000);
-
-    blog_running_years.innerHTML = years;
-    blog_running_months.innerHTML = months;
-    blog_running_days.innerHTML = days;
-    blog_running_hours.innerHTML = hours;
-    blog_running_mins.innerHTML = minutes;
-    blog_running_secs.innerHTML = seconds;
+    var time = new Date() - new Date(2022, 9, 01, 0, 0, 0);
+    var d = parseInt(time / 24 / 60 / 60 / 1000);
+    var h = parseInt((time % (24 * 60 * 60 * 1000)) / 60 / 60 / 1000);
+    var m = parseInt((time % (60 * 60 * 1000)) / 60 / 1000);
+    var s = parseInt((time % (60 * 1000)) / 1000);
+    blog_running_days.innerHTML = d;
+    blog_running_hours.innerHTML = h;
+    blog_running_mins.innerHTML = m;
+    blog_running_secs.innerHTML = s;
 }
 refresh_blog_running_time();
 if (typeof bottomTimeIntervalHasSet == "undefined") {
