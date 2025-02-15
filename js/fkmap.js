1fe2cbe0298cce6ed7d251c2eea66f93756fc15d
hao-np <2795388181@qq.com>
2025-02-15 16:34:42 +0800

    Site updated: YYYY-02-Feb 15, 2025 16:34:42

--- js/fkmap.js
+++ js/fkmap.js
@@ -1,7 +1,18 @@
+function loadClustrMapsScript() {
+    return new Promise((resolve, reject) => {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = '//clustrmaps.com/globe.js?d=xb6WYzUtwVE16HTX8X35e5GGpkywy7EXJKy_fFWi-2g';
+        script.onload = resolve;
+        script.onerror = reject;
+        document.body.appendChild(script);
+    });
+}
+
 $(document).on('pjax:complete', function() {
-    // 重新加载 ClustrMaps 脚本
-    var clustrMapsScript = document.createElement('script');
-    clustrMapsScript.type = 'text/javascript';
-    clustrMapsScript.src = '//clustrmaps.com/globe.js?d=xb6WYzUtwVE16HTX8X35e5GGpkywy7EXJKy_fFWi-2g';
-    document.body.appendChild(clustrMapsScript);
+    loadClustrMapsScript().then(() => {
+        console.log('ClustrMaps script loaded successfully');
+    }).catch(() => {
+        console.error('Failed to load ClustrMaps script');
+    });
 });
