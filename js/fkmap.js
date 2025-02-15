6f0ba4ac813742025f2809914e12e639062f0a45
hao-np <2795388181@qq.com>
2025-02-15 16:24:50 +0800

    Site updated: YYYY-02-Feb 15, 2025 16:24:50

--- js/fkmap.js
+++ js/fkmap.js
@@ -1,7 +1,7 @@
-window.onload = function() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.id = 'clstr_globe';
-    script.src = '//clustrmaps.com/globe.js?d=xb6WYzUtwVE16HTX8X35e5GGpkywy7EXJKy_fFWi-2g';
-    document.body.appendChild(script);
-};
+$(document).on('pjax:complete', function() {
+    // 重新加载 ClustrMaps 脚本
+    var clustrMapsScript = document.createElement('script');
+    clustrMapsScript.type = 'text/javascript';
+    clustrMapsScript.src = '//clustrmaps.com/globe.js?d=xb6WYzUtwVE16HTX8X35e5GGpkywy7EXJKy_fFWi-2g';
+    document.body.appendChild(clustrMapsScript);
+});
